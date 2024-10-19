7bd9c4248a9880c3f164411ca72c5b4f0396eed6
David-Llerena <dllerena@espol.edu.ec>
2024-10-18 23:17:08 -0500

    Se agrega reto 30 con su respectivo video, tener en cuenta que estan en diferentes carpetas

---
+++ EjemploLogs/src/com/krakedev/ejercicios/Admin.java
@@ -0,0 +1,19 @@
+package com.krakedev.ejercicios;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private static Logger logger = LogManager.getLogger(Admin.class);
+	public void agregar() {
+		//niveles en ese orden
+		logger.warn("Mensaje de warn");
+		logger.info("Mensaje de info");
+		logger.debug("Mensaje de debug");
+		logger.trace("Mensaje de trace");
+		
+		//siempre se ejecuta
+		logger.error("Mensaje de error");
+
+	}
+}
